9f96e619397dba00983c7ced748c14b78f2e51a6
matteen <matteen.mobasheran@uniswap.org>
2024-11-06 15:41:37 -0500

    fix polygon mapper and remove base goerli mapper (#131)
    * fix polygon mapper
    
    * remove base goerli mapper
    
    * bump version

--- src/providers/PolygonMappingProvider.ts
+++ src/providers/PolygonMappingProvider.ts
@@ -1,28 +1,36 @@
 import axios from 'axios'
 import { MappingProvider } from './MappingProvider'
-import { PolygonMappedTokenData } from '../constants/types'
+import {
+  PolygonMappedTokenData,
+  PolygonTokenListResponse,
+  PolygonWrappedToken,
+} from '../constants/types'
 
-// called from https://mapper.polygon.technology
 const url =
-  'https://api-polygon-tokens.polygon.technology/api/v1/info/all-mappings'
-const access_token = '504afd90-3228-4df9-9d88-9b4d70646101'
+  'https://api-polygon-tokens.polygon.technology/tokenlists/mapped.tokenlist.json'
+
+const POS_BRIDGE_TAG = 'pos'
 
 /**
- * The Polygon team manually maintains the mapping via user submissions at
- * https://mapper.polygon.technology.
  *
  * This provider provides the l1->l2(Polygon) token mappings.
  */
 export class PolygonMappingProvider implements MappingProvider {
   async provide(): Promise<PolygonMappedTokenData> {
-    const response = await axios.get(url, {
-      headers: { 'x-access-token': access_token },
-    })
+    const response = await axios.get<PolygonTokenListResponse>(url)
     const tokens: PolygonMappedTokenData = {}
 
-    for (const token of response.data) {
-      if (token.isPos) {
-        tokens[token.rootToken.toLowerCase()] = token
+    for (const token of response.data.tokens) {
+      const posToken = token.wrappedTokens.find(
+        (wrapped: PolygonWrappedToken) => wrapped.tags.includes(POS_BRIDGE_TAG)
+      )
+
+      if (posToken) {
+        tokens[token.originTokenAddress.toLowerCase()] = {
+          rootToken: token.originTokenAddress,
+          childToken: posToken.wrappedTokenAddress,
+          isPos: true,
+        }
       }
     }
     return tokens
